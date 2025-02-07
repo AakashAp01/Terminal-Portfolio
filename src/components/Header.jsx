2358a7db45ae71e0a9bcbc3c04ce6157376946b5
Aakash Prajapati <aakashap309@gmail.com>
2025-02-07 23:18:30 +0530

    Refactor Header component to simplify comments and improve button hover effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <header className="w-auto p-4 flex justify-center items-center">
 
-      {/* Right Side - GitHub Star Button */}
+      {/* GitHub Star Button */}
       <motion.a
         href="https://github.com/aakashap01"
         target="_blank"
         rel="noopener noreferrer"
-        className="bg-green-500  px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-green-400 transition duration-300"
+        className="bg-green-500  px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-green-500 transition duration-300"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
