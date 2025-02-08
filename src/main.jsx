822312218afa0ea5fa33473a1621bae1ed8b7a14
Aakash Prajapati <aakashap309@gmail.com>
2025-02-09 00:32:17 +0530

    Add SpeedInsights component to main entry point for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights/>
   </StrictMode>,
 )
